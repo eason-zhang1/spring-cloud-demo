76c88d47276431a741a211002eee171cedd61c16
张海军 <739663407@qq.com>
2017-06-08 16:41:32 +0800

    init

---
+++ demo2-eureka-client2/src/test/java/com/dem02/Demo2EurekaClient2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dem02;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Demo2EurekaClient2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
